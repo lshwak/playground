838e728cb6ce648e5dc8d2b12cac9198bacc42ce
lshwak <ls_hwak@naver.com>
2021-02-01 00:40:07 +0900

    html5 canvas rotating 응용

--- interactive/rt-app.js
+++ interactive/rt-app.js
@@ -32,9 +32,9 @@ class App {
 
         this.polygon = new Polygon(
             this.stageWidth / 2,
-            this.stageHeight / 2,
-            this.stageHeight / 3,
-            3
+            this.stageHeight + (this.stageHeight / 4),
+            this.stageHeight / 1.5,
+            15
         );
 
     }
