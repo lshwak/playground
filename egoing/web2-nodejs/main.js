25e2ff7c4d1537d84bcd79941748b1ecb6f72f35
lshwak <ls_hwak@naver.com>
2021-03-02 13:12:15 +0900

    egoing nodejs 글 목록 출력

--- egoing/web2-nodejs/main.js
+++ egoing/web2-nodejs/main.js
@@ -10,7 +10,7 @@ var app = http.createServer(function(request,response){
     
     // console.log(_url); // 도메인의 querystring 값이 request.url에 반영되는 것을 확인하기. url안의 값을 추출하는 것을 통해서 원하는 값을 얻을 수 있다.
     // console.log(queryData.id);
-    console.log(url.parse(_url, true));
+    // console.log(url.parse(_url, true));
 
     // if(_url == '/'){
     //   title = 'Welcome';
@@ -25,9 +25,28 @@ var app = http.createServer(function(request,response){
     // fs.readFileSync() 명령어(node.js의 기능)에 값을 주면 node.js가 경로에 해당되는 파일을 읽어서 가져오는 것.
     if(pathname === '/') {
       if(queryData.id === undefined) { // queryData 와 undefined가 같다면 홈.
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+        // fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+
+        fs.readdir('./data', function(error, filelist){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
+          
+          /*
+          var list = `<ul>
+          <li><a href="?id=HTML">HTML</a></li>
+          <li><a href="?id=CSS">CSS</a></li>
+          <li><a href="?id=JavaScript">JavaScript</a></li>
+        </ul>`
+          */
+          
+          var list = '<ul>';
+          var i = 0;
+          while(i < filelist.length){
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i = i + 1;
+          }
+          list = list + '</ul>';
+
           var template = `
           <!doctype html>
           <html>
@@ -37,11 +56,7 @@ var app = http.createServer(function(request,response){
           </head>
           <body>
             <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="?id=HTML">HTML</a></li>
-              <li><a href="?id=CSS">CSS</a></li>
-              <li><a href="?id=JavaScript">JavaScript</a></li>
-            </ul>
+            ${list}
             <h2>${title}</h2>
             <p>${description}</p>
           </body>
@@ -50,32 +65,44 @@ var app = http.createServer(function(request,response){
         // 웹 애플리케이션에서 정보를 다이나믹하게 프로그래밍적으로 생성한다. 제목부분은 title인 동적으로 만들었다.
         response.writeHead(200);
         response.end(template);
-        });
+
+        })
+
+          
+        // });
       } else {
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-          var title = queryData.id;
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="?id=HTML">HTML</a></li>
-              <li><a href="?id=CSS">CSS</a></li>
-              <li><a href="?id=JavaScript">JavaScript</a></li>
-            </ul>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
-        // 웹 애플리케이션에서 정보를 다이나믹하게 프로그래밍적으로 생성한다. 제목부분은 title인 동적으로 만들었다.
-        response.writeHead(200);
-        response.end(template);
+        fs.readdir('./data', function(error, filelist){
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var list = '<ul>';
+          var i = 0;
+          while(i < filelist.length){
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i = i + 1;
+          }
+          list = list + '</ul>';
+
+          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+            var title = queryData.id;
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>
+            `;
+          // 웹 애플리케이션에서 정보를 다이나믹하게 프로그래밍적으로 생성한다. 제목부분은 title인 동적으로 만들었다.
+          response.writeHead(200);
+          response.end(template);
+          });
         });
       }
     } else {
