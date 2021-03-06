3b50d837413b96f5466c92482dd9c591f9acd96e
lshwak <ls_hwak@naver.com>
2021-03-06 13:42:27 +0900

    egoing nodejs 함수의 입력

---
+++ egoing/web2-nodejs/syntax/function2.js
@@ -0,0 +1,13 @@
+console.log(Math.round(1.6)); //2
+console.log(Math.round(1.4)); //1
+
+function sum(first, second) { // first, second 는 parameter(매개변수)라고 한다. 
+    console.log(first + second);
+}
+sum(2,4); // 각각의 입력값을 argument(인자)라고 한다. 
+
+
+/*
+round 함수는 반올림을 해주는 함수
+round()에는 입력값이 있고 어떠한 메커니즘을 통해서 반올림은 해준다.
+*/
