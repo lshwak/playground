9f658f09b4150bf12a9f9bfd3429bd96dc434a45
lshwak <ls_hwak@naver.com>
2021-03-25 14:10:52 +0900

    object 형식

---
+++ egoing/web2-nodejs/syntax/object.js
@@ -0,0 +1,15 @@
+var members = ['egoing', 'underkg', 'itsub'];
+console.log(members[1]);
+
+var roles = {
+    'programmer':'egoing',
+    'designer':'underkg',
+    'reviewr':'itsub'
+}
+console.log(roles.reviewr);
+
+
+
+
+
+
