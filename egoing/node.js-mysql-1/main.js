a974b5503e135bf2b2be6664facb9f790b88a348
lshwak <ls_hwak@naver.com>
2021-04-12 00:59:28 +0900

    egoing nodejs mysql 글생성 기능 구현

--- egoing/node.js-mysql-1/main.js
+++ egoing/node.js-mysql-1/main.js
@@ -82,10 +82,11 @@ var app = http.createServer(function(request,response){
         });
       }
     } else if(pathname === '/create'){
-      fs.readdir('./data', function(error, filelist){
-        var title = 'WEB - create';
-        var list = template.list(filelist);
-        var html = template.HTML(title, list, `
+      db.query(`SELECT * FROM topic`, function(error,topics){
+        var title = 'Create';
+        var list = template.list(topics);
+        var html = template.HTML(title, list,
+          `
           <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
@@ -95,10 +96,13 @@ var app = http.createServer(function(request,response){
               <input type="submit">
             </p>
           </form>
-        `, '');
+          `,
+          `<a href="/create">create</a>`
+          );
         response.writeHead(200);
         response.end(html);
       });
+
     } else if(pathname === '/create_process'){
       var body = '';
       request.on('data', function(data){
@@ -106,12 +110,18 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          var title = post.title;
-          var description = post.description;
-          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-            response.writeHead(302, {Location: `/?id=${title}`});
-            response.end();
-          })
+          db.query(`
+            INSERT INTO topic (title, description, created, author_id) 
+            VALUES(?, ?, NOW(), ?)`,
+            [post.title, post.description,1],
+            function(error,result){
+              if(error){
+                throw error;
+              }
+              response.writeHead(302, {Location: `/?id=${result.insertId}`});
+              response.end();
+            }
+          )
       });
     } else if(pathname === '/update'){
       fs.readdir('./data', function(error, filelist){
