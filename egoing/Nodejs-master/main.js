ff6326c6c08088cea3c8595cf80cfb0403ffe758
lshwak <ls_hwak@naver.com>
2021-05-11 12:33:28 +0900

    egoing express 미들웨어 사용1

--- egoing/Nodejs-master/main.js
+++ egoing/Nodejs-master/main.js
@@ -3,8 +3,11 @@ var app = express() // express를 함수처럼 호출하고있다. express는 �
 var fs = require('fs');
 var path = require('path');
 var qs = require('querystring');
+var bodyParser = require('body-parser')
 var sanitizeHtml = require('sanitize-html');
 var template = require('./lib/template.js');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // get메소드는 route, routing 이라고 함.
 // app.get('/', (req, res) => res.send('Hello World!')) // 최신코드지만 아래코드와 완벽히 같다. 
@@ -65,19 +68,13 @@ app.get('/create', function(request, response){
 });
 
 app.post('/create_process', function(request, response){
-  var body = '';
-  request.on('data', function(data){
-      body = body + data;
-  });
-  request.on('end', function(){
-      var post = qs.parse(body);
-      var title = post.title;
-      var description = post.description;
-      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        response.writeHead(302, {Location: `/?id=${title}`});
-        response.end();
-      })
-  });
+  var post = request.body;
+  var title = post.title;
+  var description = post.description;
+  fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+    response.writeHead(302, {Location: `/?id=${title}`});
+    response.end();
+  })
 });
 
 app.get('/update/:pageId', function(request, response){
@@ -107,36 +104,24 @@ app.get('/update/:pageId', function(request, response){
 });
 
 app.post('/update_process', function(request, response){
-  var body = '';
-  request.on('data', function(data){
-      body = body + data;
-  });
-  request.on('end', function(){
-      var post = qs.parse(body);
-      var id = post.id;
-      var title = post.title;
-      var description = post.description;
-      fs.rename(`data/${id}`, `data/${title}`, function(error){
-        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-          response.redirect(`/?id=${title}`);
-        })
-      });
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function(error){
+    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+      response.redirect(`/?id=${title}`);
+    })
   });
 });
 
 app.post('/delete_process', function(request, response){
-  var body = '';
-  request.on('data', function(data){
-      body = body + data;
-  });
-  request.on('end', function(){
-      var post = qs.parse(body);
-      var id = post.id;
-      var filteredId = path.parse(id).base;
-      fs.unlink(`data/${filteredId}`, function(error){
-        response.redirect('/');
-      })
-  });
+    var post = request.body;
+    var id = post.id;
+    var filteredId = path.parse(id).base;
+    fs.unlink(`data/${filteredId}`, function(error){
+      response.redirect('/');
+    });
 });
 
 app.listen(3000, function() {
