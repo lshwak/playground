7b71e8a4c769ef1abe1bad199c9be5ff7da9a264
lshwak <ls_hwak@naver.com>
2021-06-03 19:16:51 +0900

    egoing express session option

--- egoing/express-session-auth-master/nodejs/express-session.js
+++ egoing/express-session-auth-master/nodejs/express-session.js
@@ -6,26 +6,13 @@ var app = express()
 
 app.use(session({
   secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true
+  resave: false, // session 데이터가 바뀌기 전까지는 session저장소의 값을 바꾸지 않는다. true면 유무 상관없이 저장.
+  saveUninitialized: true // session이 필요하기 전까지 session을 구동시키지 않는다. false면 유무 상관없이 무조건 구동.
 }))
 
-app.use(function (req, res, next) {
-  if (!req.session.views) {
-    req.session.views = {}
-  }
 
-  // get the url pathname
-  var pathname = parseurl(req).pathname
-
-  // count the views
-  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
-
-  next()
-})
-
-app.get('/foo', function (req, res, next) {
-  res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
+app.get('/', function (req, res, next) {
+  res.send('Hello Session')
 })
 
 app.get('/bar', function (req, res, next) {
