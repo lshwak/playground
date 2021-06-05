92be4e1b0db24b6fb8456e431d12419d3039c6a0
lshwak <ls_hwak@naver.com>
2021-06-05 21:30:18 +0900

    egoing express session store

--- egoing/express-session-auth-master/nodejs/express-session.js
+++ egoing/express-session-auth-master/nodejs/express-session.js
@@ -1,13 +1,15 @@
 var express = require('express')
 var parseurl = require('parseurl')
 var session = require('express-session')
+var FileStore = require('session-file-store')(session)
 
 var app = express()
 
 app.use(session({
   secret: 'keyboard cat',
   resave: false, // session 데이터가 바뀌기 전까지는 session저장소의 값을 바꾸지 않는다. true면 유무 상관없이 저장.
-  saveUninitialized: true // session이 필요하기 전까지 session을 구동시키지 않는다. false면 유무 상관없이 무조건 구동.
+  saveUninitialized: true, // session이 필요하기 전까지 session을 구동시키지 않는다. false면 유무 상관없이 무조건 구동.
+  store:new FileStore()
 }))
 
 
