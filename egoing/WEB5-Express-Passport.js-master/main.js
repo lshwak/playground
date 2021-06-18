9962451e39cb672b27e0d1b9e04667a5f0081176
lshwak <ls_hwak@naver.com>
2021-06-18 19:34:10 +0900

    egoing passport 자격확인2

--- egoing/WEB5-Express-Passport.js-master/main.js
+++ egoing/WEB5-Express-Passport.js-master/main.js
@@ -24,6 +24,12 @@ app.use(flash());
 
 // var passport = require('./lib/passport')(app);
 
+var authData = {
+  email: 'egoing777@gmail.com',
+  password: '111111',
+  nickname: 'egoing'
+};
+
 var passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy;
 
@@ -33,29 +39,27 @@ var passport = require('passport'),
       passwordField: 'pwd'
     },
     function(username, password, done){
-      //console.log('LocalStrategy', username, password);
-      /*
-      User.findOne({
-        username: username
-      }, function (err, user){
-        if (err) {
-          return done(err);
-        }
-        if(!user) {
-          return done(null, false, {
-            message: 'Incorrect username.'
+      console.log('LocalStrategy', username, password);
+      if(username === authData.email){
+        console.log(1);
+        if(password === authData.password){
+          console.log(2);
+          return done(null, authData);
+        } else {
+          console.log(3);
+          return done(null, false,{
+            message: 'Incorrent password'
           });
         }
-        if(!user.validPassword(password)) {
-          return done(null, false, {
-            message: 'Incorrect password.'
-          });
-        }
-        return done(null, user);
-      });
-      */
+      } else {
+        console.log(4);
+        return done(null, false,{
+          message: 'Incorrent username'
+        });
+      }
     }
   ));
+  // false가 아닌 값은 javascript에선 true로 친다. 성공한 사용자의 정보는 무엇인지 passport에 알려주는것.
 
 app.post('/auth/login_process',
   passport.authenticate('local', {
