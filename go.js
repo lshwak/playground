fef5012a21428bdeaea514da4c1ccdbdf98b08a9
lshwak <ls_hwak@naver.com>
2023-02-21 20:22:17 +0900

    단락회로 평가

--- go.js
+++ go.js
@@ -1,13 +1,7 @@
-let score = 70;
-
-score >= 90 ? console.log("A+") : score >=50 ? console.log("B") : console.log("F");
-
-if (score >= 90) {
-    console.log("A+");
-} else if (score >= 50) {
-    console.log("B");
-} else {
-    console.log("F");
+const getName = (person) => {
+    const name = person && person.name;
+    return name || "no object";
 }
-
-//중첩삼항연산자는 가독성이 떨어질 수 있다. 삼항 연산자와 조건식의 가독성 차이를 염두해두며 사용할 것.
+let person = null;
+const name = getName(person);
+console.log(name);
