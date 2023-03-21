186084d0ecbc8c29460f1e5904e9760a037d5276
lshwak <ls_hwak@naver.com>
2023-03-21 18:19:23 +0900

    js callback practice

--- go.js
+++ go.js
@@ -1,7 +1,28 @@
-const baseCookie = ['촉촉쿠키', '뻑뻑쿠키'];
-const topingCookie = ['초코쿠키', '라즈베리쿠키', '오트밀쿠키'];
-const allCookies = [...baseCookie, "쿠키추가요", ...topingCookie];
-
-console.log(baseCookie);
-console.log(topingCookie);
-console.log(allCookies);
+function taskA(a, b, cb) {                      
+    setTimeout(()=>{
+        const res = a + b;                     
+        cb(res);
+    },3000)                                     
+}
+function taskB(a, cb) {
+    setTimeout(()=> {
+        const res = a * 2;
+        cb(res);
+    }, 1000);
+}
+function taskC(a, cb) {
+    setTimeout(()=> {
+        const res = a * -1;
+        cb(res);
+    },2000);
+}
+taskA(4, 5, (a_res) => {                          
+    console.log("A RESULT : ", a_res);
+    taskB(a_res, (b_res)=> {
+        console.log("B RESULT : ", b_res);
+        taskC(b_res, (c_res)=> {
+            console.log("C RESULT : ", c_res);
+        })
+    })
+});
+console.log("작업 수행 끝");
