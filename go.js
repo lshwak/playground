6404d3c58b0a541b18eb908044aaf03506288acf
lshwak <ls_hwak@naver.com>
2023-03-22 17:26:35 +0900

    js promise callback practice

--- go.js
+++ go.js
@@ -1,45 +1,52 @@
-function isPositive(number, resolve, reject) {
-    setTimeout(()=>{
-        if(typeof number === "number") {
-            resolve(number >= 0 ? "양수" : "음수");
-        } else {
-            reject("NO number");
-        }
-    },2000);
-}
-
-function isPositiveP(number) {
-    const executor = (resolve, reject) => {                         //실행자
-        setTimeout(()=>{
-            if(typeof number === "number") {                        //성공 -> resolve
-                console.log(number);
-                resolve(number >= 0 ? "양수" : "음수");
-            } else {                                                //실패 -> reject
-                reject("NO number..");
-            }
-        },2000);
-    };
-    const asyncTask = new Promise(executor);
-    return asyncTask;
-}
-const res = isPositiveP(101);
-res
-    .then((res) => {
-        console.log("작업 성공 : ",res);
-    })
-    .catch((err) => {
-        console.log("작업 실패 : ", err);
+function taskA(a, b) {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=> {
+            const res = a + b;
+            resolve(res);
+        }, 3000);
     });
+}
+function taskB(a) {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=> {
+            const res = a * 2;
+            resolve(res);
+        }, 1000);
+    });
+}
+function taskC(a) {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=> {
+            const res = a * -1;
+            resolve(res);
+        }, 2000);
+    });
+}
+const bPromiseResult = taskA(5, 1).then((a_res) => {
+    console.log("A RESULT : ",a_res);
+    return taskB(a_res);
+});
+console.log("blabla");
+bPromiseResult.then((b_res) => {
+    console.log("B RESULT : ",b_res);
+    return taskC(b_res);
+})
+.then((c_res) => {
+    console.log("C RESULT : ",c_res);
+});
 
 
 
-// isPositive(
-//     10,
-//     (res) => {
-//         console.log("성공! : ", res);
-//     },
-//     (err) => {
-//         console.log("실패.. : ", err);
-//     }
-// );
 
+
+
+
+// taskA(3, 4, (a_res)=>{
+//     console.log("task A : ", a_res);
+//     taskB(a_res, (b_res)=>{
+//         console.log("task B : ", b_res);
+//         taskC(b_res, (c_res)=>{
+//             console.log("task C : ", c_res);
+//         });
+//     });
+// });
