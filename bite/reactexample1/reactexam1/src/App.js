114f5977271d5ac2b4054b1fbec1907c37e2cadb
lshwak <ls_hwak@naver.com>
2023-04-13 17:49:29 +0900

    react jsx

--- bite/reactexample1/reactexam1/src/App.js
+++ bite/reactexample1/reactexam1/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 
 function App() {
+  let name = "zunik";
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello React!</h1>
+        <h1>Hello {name} Welcome to the React!</h1>
       </header>
     </div>
   );
 }
-
 export default App;
