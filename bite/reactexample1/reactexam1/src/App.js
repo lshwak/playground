f6bda9be8ee88d76af17bb8c4d10485a141f19a0
lshwak <ls_hwak@naver.com>
2023-04-17 22:28:17 +0900

    react props

--- bite/reactexample1/reactexam1/src/App.js
+++ bite/reactexample1/reactexam1/src/App.js
@@ -2,15 +2,25 @@ import React from "react";
 //  import './App.css';
 import MyHeader from "./myHeader";
 import Counter from "./Counter";
+import Container from "./Container";
  
 function App() {
   const number = 5;
+  const counterProps = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+  }
 
   return (
+    <Container>
     <div>
       <MyHeader/>
-      <Counter />
+      <Counter {...counterProps} />
     </div>
+    </Container>
   );
 }
 export default App;
--- bite/reactexample1/reactexam1/src/Counter.js
+++ bite/reactexample1/reactexam1/src/Counter.js
@@ -1,15 +1,10 @@
 import React,{useState} from 'react'; // state는 react의 기능이라서 리액트를 임포트. useState메소드 사용.
-const Counter = () => {
-
-    // 0에서 출발
-    // 1씩 증가하고
-    // 1씩 감소하는
-    // count 상태 
-
-    console.log("counter 호출!")
+import OddEvenResult from './OddEvenResult';
+const Counter = ({inisialValue}) => {
+    //console.log(inisialValue);
 
     // 0번째 index로 count == 상태의 값으로,1번째 index로 setCount == count값을 변화시키는 상태변화함수로 사용. useState메소드 인자0은 초기값.
-    const [count,setCount] = useState(0)
+    const [count,setCount] = useState(inisialValue)
 
     // 1씩 증가하는 함수
     const onIncrease = () => {
@@ -19,26 +14,18 @@ const Counter = () => {
     const onDecrease = () => {
         setCount(count - 1);
     }
-    
-    const [count2,setCount2] = useState(0)
-
-    const onIncrease2 = () => {
-        setCount2(count2 + 1);
-    }
-    const onDecrease2 = () => {
-        setCount2(count2 - 1);
-    }
 
     return (
         <div>
             <h2>{count}</h2>   
             <button onClick={onIncrease}>+</button>
             <button onClick={onDecrease}>-</button>
-
-            <h2>{count2}</h2>   
-            <button onClick={onIncrease2}>+</button>
-            <button onClick={onDecrease2}>-</button>
+            <OddEvenResult count={count}/>
         </div>
     );
 };
+
+Counter.defaultProps = {
+    inisialValue: 0
+}
 export default Counter;
