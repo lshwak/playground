0ce9813fc49fcef01638de8daec9b5723103b560
lshwak <ls_hwak@naver.com>
2023-04-19 19:29:00 +0900

    create react Diary

---
+++ bite/simplediary/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import DiaryEditor from './DiaryEditor';
+
+function App() {
+  return (
+    <div className="App">
+      <DiaryEditor />
+    </div>
+  );
+}
+
+export default App;
---
+++ bite/simplediary/src/DiaryEditor.js
@@ -0,0 +1,12 @@
+
+
+const DiaryEditor = () => {
+
+  return  (
+    <div className="DiaryEditor">
+        <h2>오늘의 일기</h2>
+        
+    </div>
+  );
+};
+export default DiaryEditor;
