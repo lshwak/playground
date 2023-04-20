ae24b0b74eeb425b0b91a344e9b00a93c2abbd1a
lshwak <ls_hwak@naver.com>
2023-04-20 18:05:16 +0900

    react diary author&content

--- bite/simplediary/src/DiaryEditor.js
+++ bite/simplediary/src/DiaryEditor.js
@@ -1,11 +1,41 @@
-
+import { useState } from "react";
 
 const DiaryEditor = () => {
 
+  const[state, setState] = useState({
+    author:"",
+    content:""
+  });
+/**
+  const [author, setAuthor] = useState("");
+  const [content,setContent] = useState("");
+ */
   return  (
     <div className="DiaryEditor">
         <h2>오늘의 일기</h2>
-        
+        <div>
+          <input 
+          name="author"
+          value={state.author} 
+          onChange={(e) => {
+            setState({
+              author: e.target.value,
+              content: state.content
+            });
+          }}
+          />
+        </div>
+        <div>
+          <textarea 
+          value={state.content}
+          onChange={(e) => {
+            setState({
+              content: e.target.value,
+              author: state.author
+            });
+          }}
+          />
+        </div>
     </div>
   );
 };
