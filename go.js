00021f3717891da8795e01c45d89bef336d757ca
lshwak <ls_hwak@naver.com>
2023-05-08 00:37:46 +0900

    go Set & RegExp

--- go.js
+++ go.js
@@ -1,4 +1,18 @@
-function solution(num1, num2) {
-    var answer = num1 + num2;
-    return answer;
-}
+// let arr = [1, 2, 3, 4, 5, 1, 1, 2, 3, 6, 7];
+// let answer = [...new Set(arr)];
+// console.log(answer);
+
+
+
+// let users = [
+//     { id: 11, name: 'Adam', age: 23, group: 'editor' },
+//     { id: 47, name: 'John', age: 28, group: 'admin' },
+//     { id: 85, name: 'William', age: 34, group: 'editor' },
+//     { id: 97, name: 'Oliver', age: 28, group: 'admin' }
+//   ];
+
+//   let res = users.filter(it => new RegExp('oli', "i").test(it.name));
+//   console.log(res);
+
+
+
