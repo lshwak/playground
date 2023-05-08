813d98105cd06743f3e0ba9f28ade2f17255e735
lshwak <ls_hwak@naver.com>
2023-05-09 01:05:52 +0900

    go ~~ & Math.floor

--- go.js
+++ go.js
@@ -1,18 +1,8 @@
-// let arr = [1, 2, 3, 4, 5, 1, 1, 2, 3, 6, 7];
-// let answer = [...new Set(arr)];
-// console.log(answer);
+a = 7;
+b = 3;
+c = a / b ;
+console.log(c);
 
-
-
-// let users = [
-//     { id: 11, name: 'Adam', age: 23, group: 'editor' },
-//     { id: 47, name: 'John', age: 28, group: 'admin' },
-//     { id: 85, name: 'William', age: 34, group: 'editor' },
-//     { id: 97, name: 'Oliver', age: 28, group: 'admin' }
-//   ];
-
-//   let res = users.filter(it => new RegExp('oli', "i").test(it.name));
-//   console.log(res);
-
-
-
+d = ~~((a / b)*1000);
+d = Math.floor((a / b)*1000);
+console.log(d);
