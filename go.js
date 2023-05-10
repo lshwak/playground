ae8692a0d0f8bbf00a160c10acc46b0a3e015a6b
lshwak <ls_hwak@naver.com>
2023-05-11 01:21:02 +0900

    arr ave

--- go.js
+++ go.js
@@ -1,11 +1,17 @@
-let a = 10;
-let b = 4;
-let answer = 0;
+const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const b = [89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99];
 
-for (i=1; i <= a; i++) {
-    // if (i % 2 == 0){
-    //     answer += i;
-    // }
-    i % 2 == 0 ? answer += i :  answer+=0 ;
-}
-console.log(answer);
+answer = 0;
+bnswer = 0;
+
+// for (i=0; i <= 10; i++) {
+//     answer += i;
+// }
+
+a.map((a) => answer+=a);
+b.map(b => bnswer+=b);
+answer = answer/a.length;
+bnswer = bnswer/b.length;
+
+console.log(answer.toFixed(1));
+console.log(bnswer.toFixed(1));
