d6bd235b8d6ea4fe9f920f2bdbb7b430c60d53b4
lshwak <ls_hwak@naver.com>
2023-05-17 23:17:30 +0900

    go

--- go.js
+++ go.js
@@ -1,61 +1,13 @@
+let num1 = [1, 2, 3, 4, 5];
+let num2 = [1, 1, 1, 1, 1, 2];
+let num3 = [1, 0, 1, 1, 1, 3, 5];
 
-let stick = 12000;
-let drink = 2000;
+let temp = [];
 
-const n1 = 10;
-const k1 = 3;
-let count1 = 0;
+answer1 =
+answer2 = 
+answer3 = 
 
-const n2 = 64;
-const k2 = 6;
-let count2 = 0;
-
-let answer = 0;
-
-for (i=1; i <= n1; i ++) {
-    for (j=1; j <= k1; j++) {
-        a = drink * k1;
-    }
-    b = stick*i;
-    if (i % 10 == 0) {
-        count1++;
-    }
-}
-
-console.log(count1);
-console.log("꼬치 : "+b);
-console.log("음료 : "+a);
-console.log(b+(a-(count1*drink)));
-
-
-for (i=1; i <= n2; i ++) {
-    for (j=1; j <= k2; j++) {
-        c = drink * k2;
-    }
-    d = stick*i;
-    if (i % 10 == 0) {
-        count2++;
-    }
-}
-console.log(count2);
-console.log("꼬치 : "+d);
-console.log("음료 : "+c);
-console.log(d+(c-(count2*drink)));
-
-
-
-
-
-let k = 10;
-let n = 3;
-    k-=~~(n/10);
-    if (k < 0) k= 0;
-answer = n*12000+k*2000;
-console.log("다른사람 풀이 : "+answer);
-
-let time1 = new Date();
-let time2 = new Date().getTime();
-let time3 = new Date().toLocaleDateString();
-console.log(time1);
-console.log(time2);
-console.log(time3);
+console.log(answer1);
+console.log(answer2);
+console.log(answer3);
