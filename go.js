c1d3d9a2ad8359f70b18f416d934cfa301279673
lshwak <ls_hwak@naver.com>
2023-05-24 13:50:37 +0900

    go

--- go.js
+++ go.js
@@ -1,11 +1,6 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12926 프로그래머스 시저암호 
 
-
-
 //65 ~ 90 & 97 ~ 122
-// tem = ((65 <= tem && tem <= 90) || (97 <= tem && tem <= 122)) ? 
-// temp = tem + n : temp = tem;
-
 
 let s = "a B z";
 let n = 4;
@@ -31,51 +26,5 @@ let answer = '';
         answer += String.fromCharCode(temp);
     }
     
-
 console.log(answer);
 
-
-
-
-
-
-
-// let eng = "ABC";
-// let num1 = 65;
-// let num2 = 66;
-// let num3 = 67;
-
-
-// console.log(eng.charCodeAt(0));
-// console.log(eng.charCodeAt(1));
-// console.log(eng.charCodeAt(2));
-// console.log(String.fromCharCode(65));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let num1 = [1, 2, 3, 4, 5];
-// let num2 = [1, 1, 1, 1, 1, 2];
-// let num3 = [1, 0, 1, 1, 1, 3, 5];
-
-// let temp = [];
-
-// answer1 =
-// answer2 = 
-// answer3 = 
-
-// console.log(answer1);
-// console.log(answer2);
-// console.log(answer3);
