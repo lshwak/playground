f8ce4b45ea5a48255eca38d4c4463378fbb91629
lshwak <ls_hwak@naver.com>
2023-05-26 00:35:15 +0900

    go

--- go.js
+++ go.js
@@ -1,25 +1,19 @@
-let age = 25;           // 정수
-let tall = 175.9;       // 실수
+let zunik = {
+    name: "zunik",
+    age: 31,
+    job: "developer",
+    secondJob : "bboy"
+}
 
-let inf = Infinity;         // 무한대
-let minusInf = -Infinity;   // 음의 무한데
-let nan = NaN;              // 실패의 결과값
+for(let [key,value] of Object.entries(zunik)) {
+    console.log(`${key}:${value}`);
+}
 
-let name = "zunik";         // 쌍따옴표
-let bboy = 'bboy';          // 작은 따옴표
-let street = `hiphop ${bboy} ${name}`;      // 백틱 (변수의 값을 추가가능/ 템플릿 리터럴)
+let user = new Map ();
+user.set ("name", "zunik");
+user.set ("age", "30");
+user.set ("from", "ulsan");
 
-let isSwitchOff = false;
-
-let a = null;
-
-let A = 12;
-let B = "2";
-
-console.log( age + tall);
-console.log( age * tall);
-console.log(street);
-console.log(a);
-console.log(A * B);             // 묵시적 형변환, 캐스팅.
-console.log(A + B);
-console.log(A + parseInt(B));   // parseInt로 명시적 형변환.
+for (let [key,value] of user) {
+    console.log(`${key}:${value}`)
+}
