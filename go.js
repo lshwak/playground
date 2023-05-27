fec53e43c1abf1544761a4b4adc57ebdaf096df7
lshwak <ls_hwak@naver.com>
2023-05-27 21:39:32 +0900

    go cote aeiou

--- go.js
+++ go.js
@@ -1,12 +1,15 @@
+// a, e, i, o, u 
+const my_string = "nice to meet you";
 
-function solution(my_string, letter) {
-    var answer = '';
-    //let a = '';
-    for (i=0; i < my_string.length; i++) {
-        if (my_string[i] != letter) {
-            answer += my_string[i];
+    let answer1 = '';
+    answer1 = my_string.replace(/[a,e,i,o,u]/g,'');
+    console.log(answer1);
+
+    let answer2 = '';
+    for (i=0; i<my_string.length; i++) {
+        if (my_string[i] !=  "a" && my_string[i] != "e" && my_string[i] != "i" && my_string[i] != "o" && my_string[i] != "u") {
+            answer2 += my_string[i];
         }
     }
-    //answer = my_string.split(letter).join(a);
-    return answer;
-}
+    console.log(answer2);
+
