40bdda70ca784a2b6c8d362f438ab48314670a46
lshwak <ls_hwak@naver.com>
2023-05-30 00:57:09 +0900

    coding test ant army

--- go.js
+++ go.js
@@ -1,14 +1,15 @@
-const my_string = "nice to meet you";
-
-    let answer1 = '';
-    answer1 = my_string.replace(/[a,e,i,o,u]/g,'');
-    console.log(answer1);
-
-    let answer2 = '';
-    for (i=0; i<my_string.length; i++) {
-        if (my_string[i] !=  "a" && my_string[i] != "e" && my_string[i] != "i" && my_string[i] != "o" && my_string[i] != "u") {
-            answer2 += my_string[i];
-        }
+let hp = 24;
+let answer = 0;
+    while (hp >= 5) {
+        hp -= 5;
+        answer++
     }
-    console.log(answer2);
-
+    while (hp >= 3) {
+        hp -= 3;
+        answer++
+    }
+    while (hp > 0) {
+        hp -= 1;
+        answer ++
+    }
+console.log(answer)
