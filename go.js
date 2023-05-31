312699779fc2c4e430f8c69c22ab694a4eeb58f1
lshwak <ls_hwak@naver.com>
2023-05-31 17:10:11 +0900

    cote ants army

--- go.js
+++ go.js
@@ -1,15 +1,13 @@
 let hp = 24;
 let answer = 0;
-    while (hp >= 5) {
-        hp -= 5;
-        answer++
-    }
-    while (hp >= 3) {
-        hp -= 3;
-        answer++
-    }
     while (hp > 0) {
-        hp -= 1;
-        answer ++
+        if (hp >= 5) {
+            hp -= 5;
+        } else if (hp >= 3) {
+            hp -= 3;
+        } else {
+            hp -= 1;
+        }
+        answer++;
     }
 console.log(answer)
