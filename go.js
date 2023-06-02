04d08d872685fb483b4eacceb073d578789b4ac8
lshwak <ls_hwak@naver.com>
2023-06-03 01:02:32 +0900

    cote rectangle area

--- go.js
+++ go.js
@@ -1,20 +1,19 @@
-babbling = ["ayaye", "uuuma", "ye", "yemawoo", "ayaa"];
+// 직사각형 넚이 구하기
+const dots = [[1, 1], [5, 1], [5, 5], [1, 5]];
 
-var answer = 0;
-let  str = ["aya","ye","woo","ma"];
-    
-    for (i=0; i<babbling.length; i++) {
-        let a = babbling[i]                 // 변수에 담아 필터링
-        for (j=0; j<str.length; j++) {
-            if (babbling[i].includes(str[j])) {
-                if (a.includes(str[j])) {
-                    a = a.replace(str[j],'');
-                }
-            }
-        }   
-        if (a == '') {
-            answer++
-        }
+    let x = [];
+    let y = [];
+    let xb = 0;
+    let xs = 0;
+    let yb = 0;
+    let ys = 0;
+    for (i=0; i<dots.length; i++) {
+        x.push(dots[i][0]);         // x 좌표만 배열로 추가
+        y.push(dots[i][1]);         // y 좌표만 배열로 추가
     }
-
-    console.log(answer);
+    xb = Math.max.apply(null,x)     // x 최대값
+    xs = Math.min.apply(null,x)     // x 최소값
+    yb = Math.max.apply(null,y)     // y 최대값
+    ys = Math.min.apply(null,y)     // y 최소값
+    
+    console.log(((xb - xs) * (yb - ys))) // x길이 * y길이 == 넓이
