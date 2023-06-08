1f36b045f3bd5c76c36c3eda5d7254bf1ee90d92
lshwak <ls_hwak@naver.com>
2023-06-08 18:23:30 +0900

    bite emotion-diary react router 응용

--- bite/emotion-diary/src/pages/Edit.js
+++ bite/emotion-diary/src/pages/Edit.js
@@ -1,8 +1,23 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+
+    const navigate = useNavigate();
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const id = searchParams.get("id");
+    console.log("id : ", id);
+
+    const mode = searchParams.get("mode");
+    console.log("mode : ", mode);
+
     return (
         <div>
             <h1>Edit</h1>
             <p>일기 수정 페이지</p>
+            <button onClick={()=>setSearchParams({who: "zunik"})}>QS 바꾸기</button>
+            <button onClick={()=>{navigate("/home")}}>HOME으로 가기</button>
+            <button onClick={()=>{navigate(-1)}}>뒤로가기</button>
         </div>
     );
 };
