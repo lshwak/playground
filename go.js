507ac1d641e03be3909c619bc269f58e27261093
lshwak <ls_hwak@naver.com>
2023-06-10 22:25:39 +0900

    go

--- go.js
+++ go.js
@@ -2,6 +2,7 @@
     const my_string = "hello";
     const n = 3;
     let answer = '';
+    
     for (let i=0; i<my_string.length; i++) {
         for (let j=0; j<n; j++) {
             answer += my_string[i];
@@ -12,9 +13,9 @@
     const mystr = "whatup";
     const m = 5;
     let ans = '';
-
     let arr = Array.from(mystr);
     let str = [];
+
     for(i=0; i<arr.length; i++) {
         for (j=1; j<=m; j++) {
             str.push(arr[i])    
