8596802433b1f4ebf0f17648f0c6c72f20a66d0e
lshwak <ls_hwak@naver.com>
2023-06-17 14:43:22 +0900

    bite emotion diary Home Header

--- bite/emotion-diary/src/pages/Home.js
+++ bite/emotion-diary/src/pages/Home.js
@@ -1,8 +1,31 @@
+import { useState } from "react";
+
+import MyHeader from "./../components/MyHeader";
+import MyButton from "./../components/MyButton";
+
+
 const Home = () => {
+    const [curDate, setCurDate] = useState(new Date());
+    const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`
+
+    const increaseMonth = () => {
+        setCurDate(
+            new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+        );
+    };
+
+    const decreaseMonth = () => {
+        setCurDate(
+            new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+        );
+    };
+
     return (
         <div>
-            <h1>Home</h1>
-            <p>홈페이지</p>
+            <MyHeader headText={headText}
+            leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
+            rightChild={<MyButton text={">"} onClick={increaseMonth} />}
+            />
         </div>
     );
 };
