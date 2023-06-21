72187de48bba26e20543e7b70a35f93dee40c381
lshwak <ls_hwak@naver.com>
2023-06-21 20:32:44 +0900

    bite emotion diary emotion date content edit button

--- bite/emotion-diary/src/components/DiaryList.js
+++ bite/emotion-diary/src/components/DiaryList.js
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import { useNavigate } from "react-router-dom";
 import MyButton from "./MyButton";
+import DiaryItem from "./DiaryItem";
 
 const ControlMenu = ({value, onChange, optionList}) => {
     return (
@@ -70,7 +71,7 @@ const DiaryList = ({diaryList}) => {
                 </div>
             </div>
             {getProcessedDiaryList().map((it)=> (
-                <div key={it.id}>{it.content}{it.emotion}</div>
+                <DiaryItem key={it.id} {...it} />
             ))}
         </div>
     );
