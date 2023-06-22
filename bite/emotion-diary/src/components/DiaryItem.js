f2cf9d9c64ab5970165fccb237d363e052d5165f
lshwak <ls_hwak@naver.com>
2023-06-22 14:28:57 +0900

    bite emotion diary goDetail & goEdit

--- bite/emotion-diary/src/components/DiaryItem.js
+++ bite/emotion-diary/src/components/DiaryItem.js
@@ -1,20 +1,31 @@
+import { Navigate, useNavigate } from "react-router-dom";
 import MyButton from "./MyButton";
 
 const DiaryItem = ({id, emotion, content, date}) => {
     // const env = process.env;
     // env.PUBLIC_URL = env.PUBLIC_URL || "";
 
+    const navigate = useNavigate();
+
     const strDate = new Date(parseInt(date)).toLocaleDateString();
 
+    const goDetail = () => {
+        navigate(`/diary/${id}`);
+    }
+
+    const goEdit = () => {
+        navigate(`/edit/${id}`);
+    }
+
     return <div className="DiaryItem">
-        <div className={["emotion_img_wrapper", `emotion_img_wrapper_${emotion}`].join(" ")}>
+        <div onClick={goDetail} className={["emotion_img_wrapper", `emotion_img_wrapper_${emotion}`].join(" ")}>
             <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} />
         </div>
-        <div className="info_wrapper">
+        <div onClick={goDetail} className="info_wrapper">
             <div className="diary_date">{strDate}</div>
             <div className="diary_content_preview">{content.slice(0, 25)}</div>
         </div>
-        <div className="btn_wrapper">
+        <div onClick={goEdit} className="btn_wrapper">
             <MyButton text={"수정하기"} />
         </div>
     </div>
