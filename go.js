b36cb765a5e9aa9974616774fc04a1363f4d74e7
lshwak <ls_hwak@naver.com>
2023-07-13 18:58:07 +0900

    go

--- go.js
+++ go.js
@@ -1,3 +1,11 @@
-mystring = "We are the world";
+const array = [1, 9, 10, 11, 8];
+let h = Math.max(...array);
+let idx = array.indexOf(h);
 
-console.log([...new Set(mystring)].join(''));
+const answer = [];
+answer.push(h,idx)
+
+console.log(h)
+console.log(idx)
+
+console.log(answer)
