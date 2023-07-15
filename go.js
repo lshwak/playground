5d5746e5c482e063327d4141941213f1d83ae1ca
lshwak <ls_hwak@naver.com>
2023-07-16 01:01:30 +0900

    go switch case

--- go.js
+++ go.js
@@ -1,10 +1,31 @@
-const array = [1, 9, 10, 11, 8];
-let h = Math.max(...array);
-let idx = array.indexOf(h);
+let num = 10;
 
-const answer = [];
-answer.push(h,idx)
+    let day = '';
 
-console.log(h)
-console.log(idx)
-console.log(answer)
+    switch(num) {
+        case 1 :
+            day = '월요일';
+        break;
+        case 2 :
+            day = '화요일';
+        break;
+        case 3 :
+            day = '수요일';
+        break;
+        case 4 :
+            day = '목요일';
+        break;
+        case 5 :
+            day = '금요일';
+        break;
+        case 6 :
+            day = '토요일';
+        break;
+        case 7 :
+            day = '일요일';
+        break;
+        default :
+        day = '1부터 7까지의 숫자가 아님';
+    }
+
+console.log(day);
