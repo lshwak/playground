f797f7e2ead8c9e8a3bd5b3940a95670198c43a3
lshwak <ls_hwak@naver.com>
2024-01-23 01:41:43 +0900

    go

--- go.js
+++ go.js
@@ -5,13 +5,16 @@ const p = [["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["ko
 let a = []
 let b
 
+
 for (let i = 0; i < p.length; i++) {
   b = (n.filter(x => p[i].includes(x)))
   console.log(b)
   for (let j = 0; j < b.length; j++) {
-    console.log(b[j])
+    //console.log(b[j])
     for (let k = 0; k < n.length; k++) {
-      
+      if (n[k] == b[j]) {
+        //console.log(n.indexOf(b[j]))
+      }
     }
   }
 }
