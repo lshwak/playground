906439406de6a07e3c2442ea4ae3a140dc577d79
lshwak <ls_hwak@naver.com>
2024-01-27 09:50:57 +0900

    go

--- go.js
+++ go.js
@@ -1,61 +1,27 @@
-const array = [1, 5, 2, 6, 3, 7, 4];
-const commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]];
+let result = "";
 
-let ans = [];
-let arr = [];
-
-for (let i=0; i<commands.length; i++) {
-  arr = array.slice(commands[i][0]-1, commands[i][1]).sort((a,b) => a - b);
-  let a = commands[i][2]-1
-  ans.push(arr[a]);
+for (let i=1; i<=9; i++) {
+  for (let j=2; j<=5; j++) {
+    if (i !== 4) {
+      result += j + " * " + i + " = " + j * i + "\t";
+    }
+  }
+  if (i !== 4) {
+    result += "\n";
+  }
 }
-console.log(ans);
 
+result += "\n";
 
+for (let i=1; i<=9; i++) {
+  for (let j=6; j<=9; j++) {
+    if (i !== 4) {
+      result += j + " * " + i + " = " + j * i + "\t";
+    }
+  }
+  if (i !== 4) {
+    result += "\n";
+  }
+}
 
-
-
-
-// const n = ["may", "kein", "kain", "radi"];
-// const y = [5, 10, 1, 3];
-// const p = [["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]];
-
-// let a = []
-// let b
-
-
-// for (let i = 0; i < p.length; i++) {
-//   b = (n.filter(x => p[i].includes(x)))
-//   console.log(b)
-//   for (let j = 0; j < b.length; j++) {
-//     //console.log(b[j])
-//     for (let k = 0; k < n.length; k++) {
-//       if (n[k] == b[j]) {
-//         //console.log(n.indexOf(b[j]))
-//       }
-//     }
-//   }
-// }
-// //console.log(a)
-// //console.log(b.map(x => x))
-
-
-// const arr = [1, 2, 3, 4, 5]
-  
-// arr.reduce(function (acc, cur, idx) {
-//   console.log(acc, cur, idx);
-//   return acc + cur;
-// }, 0);
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(result)
